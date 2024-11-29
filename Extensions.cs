8210be7ec895b366d9efcb85528d117d3de3d1f0
tolltech <alexandrovpe@gmail.com>
2024-11-29 11:44:49 +0400

    firstblood

---
+++ Extensions.cs
@@ -0,0 +1,137 @@
+﻿using System.Text;
+
+namespace AoC_2024;
+
+    public static class Extensions
+    {
+        public static int ToIntFromBin(this string src)
+        {
+            return Convert.ToInt32(src, 2);
+        }
+
+        public static long ToLongFromBin(this string src)
+        {
+            return Convert.ToInt64(src, 2);
+        }
+
+        private static readonly Dictionary<char, string> bin = new Dictionary<char, string>
+        {
+            { '0', "0000" },
+            { '1', "0001" },
+            { '2', "0010" },
+            { '3', "0011" },
+            { '4', "0100" },
+            { '5', "0101" },
+            { '6', "0110" },
+            { '7', "0111" },
+            { '8', "1000" },
+            { '9', "1001" },
+            { 'A', "1010" },
+            { 'B', "1011" },
+            { 'C', "1100" },
+            { 'D', "1101" },
+            { 'E', "1110" },
+            { 'F', "1111" },
+        };
+
+        public static string ToBinFromHex(this string src)
+        {
+            var sb = new StringBuilder();
+
+            foreach (var c in src)
+            {
+                sb.Append(bin[c]);
+            }
+
+            return sb.ToString().TrimStart('0');
+        }
+
+        public static HashSet<T> ToHashSet<T>(this IEnumerable<T> ts)
+        {
+            return new HashSet<T>(ts);
+        }
+
+        public static int MaxOrDefault<T>(this IEnumerable<T> src, Func<T, int> pred)
+        {
+            var array = src.ToArray();
+            return !array.Any() ? 0 : array.Max(pred);
+        }
+
+        public static TValue SafeGet<TKey, TValue>(this IDictionary<TKey, TValue> dict, TKey key, TValue val = default)
+        {
+            return dict.TryGetValue(key, out var v) ? v : val;
+        }
+
+        public static string[] SplitEmpty(this string str, params string[] separators)
+        {
+            return str.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        public static string[] SplitLines(this string str, params string[] separators)
+        {
+            return str.SplitEmpty("\r", "\n");
+        }
+
+        public static IEnumerable<(T Item, (int Row, int Col) Index)> GetAllNeighbours<T>(T[][] map,
+            (int Row, int Col) src)
+        {
+            for (var i = -1; i <= 1; ++i)
+            for (var j = -1; j <= 1; j++)
+            {
+                if (j == 0 && i == 0) continue;
+
+                var row = src.Row + i;
+                var col = src.Col + j;
+
+                if (row < 0 || col < 0 || row >= map.Length || map.Length == 0 || col >= map[0].Length) continue;
+
+                yield return (map[row][col], (row, col));
+            }
+        }
+        
+        public static IEnumerable<(T Item, (int Row, int Col) Index)> GetVerticalHorizontalNeighbours<T>(T[][] map,
+            (int Row, int Col) src)
+        {
+            for (var i = -1; i <= 1; ++i)
+            for (var j = -1; j <= 1; j++)
+            {
+                if (j == 0 && i == 0) continue;
+                if (j != 0 && i != 0) continue;
+
+                var row = src.Row + i;
+                var col = src.Col + j;
+
+                if (row < 0 || col < 0 || row >= map.Length || map.Length == 0 || col >= map[0].Length) continue;
+
+                yield return (map[row][col], (row, col));
+            }
+        }
+        
+        public static IEnumerable<(T Item, (int Row, int Col) Index, (int, int) Direction)> GetVerticalHorizontalNeighboursDirections<T>(T[][] map,
+            (int Row, int Col) src)
+        {
+            for (var i = -1; i <= 1; ++i)
+            for (var j = -1; j <= 1; j++)
+            {
+                if (j == 0 && i == 0) continue;
+                if (j != 0 && i != 0) continue;
+
+                var row = src.Row + i;
+                var col = src.Col + j;
+
+                if (row < 0 || col < 0 || row >= map.Length || map.Length == 0 || col >= map[0].Length) continue;
+
+                yield return (map[row][col], (row, col), (i, j));
+            }
+        }
+
+        public static string JoinToString<T>(this IEnumerable<T> items, string separator = "")
+        {
+            return string.Join(separator, items);
+        }
+        
+        public static string GetLastOrEmpty(this string src)
+        {
+            return src.Length > 0 ? src.Last().ToString() : string.Empty;
+        }
+    }
---
+++ Task01.cs
@@ -0,0 +1,29 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace AoC_2024;
+
+[TestFixture]
+public class Task01
+{
+    [Test]
+    [TestCase(
+        @"1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet",
+        142)]
+    [TestCase(@"Task01.txt", 55029)]
+    public void Task(string input, int expected)
+    {
+        input = File.Exists(input) ? File.ReadAllText(input) : input;
+
+        var result = 0;
+        foreach (var line in input.Split(new[]{"\r", "\n"}, StringSplitOptions.RemoveEmptyEntries))
+        {
+            result += int.Parse($"{line.First(char.IsDigit)}{line.Last(char.IsDigit)}");                
+        }
+
+        result.Should().Be(expected);
+    }
+}
