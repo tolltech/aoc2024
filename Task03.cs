79981b35cc792b66486b209133a9f5d82643b103
tolltech <alexandrovpe@gmail.com>
2024-12-03 11:35:11 +0400

    task 03

---
+++ Task03.cs
@@ -0,0 +1,44 @@
+﻿using System.Text.RegularExpressions;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace AoC_2024;
+
+[TestFixture]
+public class Task03
+{
+    [Test]
+    [TestCase(
+        @"xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))",
+        161)]
+    [TestCase(@"Task03.txt", 0)]
+    public void Task(string input, int expected)
+    {
+        input = File.Exists(input) ? File.ReadAllText(input) : input;
+
+        var result = 0;
+
+        var matches = Regex.Matches(input, @"mul\(\d{1,3},\d{1,3}\)");
+
+        foreach (Match match in matches)
+        {
+            var splits = match.Value.Split(",", StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => new string(x.Where(char.IsDigit).ToArray()))
+                .Select(int.Parse)
+                .ToArray();
+
+            result += splits[0] * splits[1];
+        }
+
+        result.Should().Be(expected);
+    }
+
+    private static bool CheckSafe(int delta, int? prevDelta)
+    {
+        if (Math.Abs(delta) > 3) return false;
+        if (delta == 0) return false;
+        if (prevDelta.HasValue && Math.Sign(prevDelta.Value) != Math.Sign(delta)) return false;
+
+        return true;
+    }
+}
