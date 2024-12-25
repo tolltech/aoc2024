e08b8dd8c46ff3d82da18f0a25656de802e68e6f
tolltech <alexandrovpe@gmail.com>
2024-12-25 16:27:41 +0400

    21 2

--- Task21_2.cs
+++ Task21_2.cs
@@ -32,8 +32,9 @@ public class Task21_2
             var suitableCommands = new List<string>();
             foreach (var expectedChar in line)
             {
-                var minCommand = GetCommands(expectedChar, commands, suitableCommands).Distinct().OrderBy(x => x.Length).ToArray();
-            
+                var minCommand = GetCommands(expectedChar, commands, suitableCommands).Distinct().OrderBy(x => x.Length)
+                    .ToArray();
+
                 suitableCommands.Add(minCommand.First());
             }
 
@@ -41,7 +42,7 @@ public class Task21_2
 
             result += ((long)resultCommand.Length) * int.Parse(new string(line.Where(char.IsDigit).ToArray()));
         }
-        
+
         result.Should().Be(expected);
     }
 
@@ -53,26 +54,26 @@ public class Task21_2
         foreach (var command4 in commands)
         {
             var keyBoard = InitKeyboards();
-            
+
             foreach (var command in suitableCommands)
             {
                 ExecCommand(command, keyBoard);
             }
-            
+
             var bigCommand = command1 + command2 + command3 + command4;
-            
+
             for (var i = 0; i < bigCommand.Length; i++)
             {
                 var c = bigCommand[i];
-                
+
                 var r = keyBoard.Exec(c);
-                
+
                 if (r == target)
                 {
                     yield return bigCommand.Substring(0, i + 1);
                     break;
                 }
-                
+
                 if (r != default) break;
             }
         }
@@ -82,48 +83,43 @@ public class Task21_2
     // ^<<1^^7>>9vvvA
     // ^^<<4>5>6vvA
     // ^3<<^^7>>9vvvA
-    
+
     // 179A: <v<A>>^A<vA<A>>^AAvAA<^A>A<v<A>>^AAvA^A<vA>^AA<A>A<v<A>A>^AAAvA<^A>A
     // 456A: <v<A>>^AA<vA<A>>^AAvAA<^A>A<vA>^A<A>A<vA>^A<A>A<v<A>A>^AAvA<^A>A
     // 379A: <v<A>>^AvA^A<vA<AA>>^AAvA<^A>AAvA^A<vA>^AA<A>A<v<A>A>^AAAvA<^A>A
-    
+
     [Test]
     // [TestCase("029A","<A^A>^^AvvvA", "<vA<AA>>^AvAA<^A>A<v<A>>^AvA^A<vA>^A<v<A>^A>AAvA^A<v<A>A>^AAAvA<^A>A")]
     // [TestCase("980A","^^^A<AvvvA>A", "<v<A>>^AAAvA^A<vA<AA>>^AvAA<^A>A<v<A>A>^AAAvA<^A>A<vA>^A<A>A")]
     // [TestCase("179A","^<<A^^A>>AvvvA", "<v<A>>^A<vA<A>>^AAvAA<^A>A<v<A>>^AAvA^A<vA>^AA<A>A<v<A>A>^AAAvA<^A>A")]
     // [TestCase("456A","^^<<A>A>AvvA", "<v<A>>^AA<vA<A>>^AAvAA<^A>A<vA>^A<A>A<vA>^A<A>A<v<A>A>^AAvA<^A>A")]
     // [TestCase("379A","^A<<^^A>>AvvvA", "<v<A>>^AvA^A<vA<AA>>^AAvA<^A>AAvA^A<vA>^AA<A>A<v<A>A>^AAAvA<^A>A")]
-    
+
     //^<<1^4>vv0>A
     // ^<<1^>>6^9vvvA
     // ^<<1^^7>vvv0>A
     // ^^<5v2^^8vvv>A
     // ^3^<<4>vv0>A
-    
-    [TestCase("140A","^<<A^A>vvA>A", 70)]//70 !!! 9800
-    [TestCase("140A","^<<A^Av>vA>A", 76)]//76
-    
-    [TestCase("169A","^<<A^>>A^AvvvA", 76)]//76 !!! 12844
-    [TestCase("169A","^<<A>^>A^AvvvA", 82)]//82
-    [TestCase("169A","^<<A>>^A^AvvvA", 76)]//76
-    
-    [TestCase("170A","^<<A^^A>vvvA>A", 72)]//72 !!! 12240
-    [TestCase("170A","^<<A^^Avv>vA>A", 78)]//78
-    [TestCase("170A","^<<A^^Av>vvA>A", 78)]//78
-    
-    [TestCase("528A","^^<AvA^^Avvv>A", 74)]//74
-    [TestCase("528A","<^^AvA^^Avvv>A", 70)]//70 !!! 36960
-    [TestCase("528A","<^^AvA^^A>vvvA", 74)]//74
-    [TestCase("528A","<^^AvA^^Av>vvA", 80)]//80
-    [TestCase("528A","<^^AvA^^Avv>vA", 80)]//80
-    [TestCase("528A","^^<AvA^^Avv>vA", 84)]//84
-    
-    [TestCase("340A","^A^<<A>vvA>A", 70)]//70 
-    [TestCase("340A","^A<<^A>vvA>A", 66)]// 66 !!! 22440
-    [TestCase("340A","^A<^<A>vvA>A", 78)]// 78
-    [TestCase("340A","^A^<<Av>vA>A", 76)]//76
+    [TestCase("140A", "^<<A^A>vvA>A", 70)] //70 !!! 9800
+    [TestCase("140A", "^<<A^Av>vA>A", 76)] //76
+    [TestCase("169A", "^<<A^>>A^AvvvA", 76)] //76 !!! 12844
+    [TestCase("169A", "^<<A>^>A^AvvvA", 82)] //82
+    [TestCase("169A", "^<<A>>^A^AvvvA", 76)] //76
+    [TestCase("170A", "^<<A^^A>vvvA>A", 72)] //72 !!! 12240
+    [TestCase("170A", "^<<A^^Avv>vA>A", 78)] //78
+    [TestCase("170A", "^<<A^^Av>vvA>A", 78)] //78
+    [TestCase("528A", "^^<AvA^^Avvv>A", 74)] //74
+    [TestCase("528A", "<^^AvA^^Avvv>A", 70)] //70 !!! 36960
+    [TestCase("528A", "<^^AvA^^A>vvvA", 74)] //74
+    [TestCase("528A", "<^^AvA^^Av>vvA", 80)] //80
+    [TestCase("528A", "<^^AvA^^Avv>vA", 80)] //80
+    [TestCase("528A", "^^<AvA^^Avv>vA", 84)] //84
+    [TestCase("340A", "^A^<<A>vvA>A", 70)] //70 
+    [TestCase("340A", "^A<<^A>vvA>A", 66)] // 66 !!! 22440
+    [TestCase("340A", "^A<^<A>vvA>A", 78)] // 78
+    [TestCase("340A", "^A^<<Av>vA>A", 76)] //76
     //94284
-    [TestCase("1","^<<A", 26)]//v<<A>>^Av<A<A>>^AAvAA<^A>A
+    [TestCase("1", "^<<A", 26)] //v<<A>>^Av<A<A>>^AAvAA<^A>A
     public void Command(string expectedOutput, string command, long expected)
     {
         var commands = command.Split('A');
@@ -138,7 +134,7 @@ public class Task21_2
 
         result.Should().Be(expected);
     }
-    
+
     [Test]
     [TestCase(2, 94284)]
     [TestCase(25, 0)]
@@ -156,10 +152,10 @@ public class Task21_2
             }
             : new[]
             {
-                ("029A", "<A^A^^>AvvvA"), 
-                ("980A", "^^^A<AvvvA>A"), 
-                ("179A", "^<<A^^A>>AvvvA"), 
-                ("456A", "^^<<A>A>AvvA"), 
+                ("029A", "<A^A^^>AvvvA"),
+                ("980A", "^^^A<AvvvA>A"),
+                ("179A", "^<<A^^A>>AvvvA"),
+                ("456A", "^^<<A>A>AvvA"),
                 ("379A", "^A^^<<A>>AvvvA"),
             };
 
@@ -167,9 +163,11 @@ public class Task21_2
 
         var resultDict = new Dictionary<string, long>();
         var minCommands = new Dictionary<string, string>();
-        
+
         var cache = new Dictionary<(string, int), long>();
-        
+
+        CommandsMutated = Promote();
+
         foreach (var tuple in input)
         {
             var commands = tuple.Item2.Split('A');
@@ -198,8 +196,66 @@ public class Task21_2
 
         realResult.Should().Be(expected);
     }
-    
-    
+
+    private Dictionary<string, HashSet<string>> Promote()
+    {
+        var result = new Dictionary<string, HashSet<string>>();
+
+        result[""] = ["A"];
+
+        var baseChars = new[] { '<', '>', '^', 'v' };
+        var srcCommands = new HashSet<string>();
+
+        foreach (var c1 in baseChars)
+        foreach (var c2 in baseChars)
+        foreach (var c3 in baseChars)
+        foreach (var c4 in baseChars)
+        {
+            srcCommands.Add(c1.ToString() + c2 + c3 + c4);
+            srcCommands.Add(c1.ToString() + c2 + c3);
+            srcCommands.Add(c1.ToString() + c2);
+            srcCommands.Add(c1.ToString());
+        }
+
+        foreach (var srcCommand in srcCommands)
+        {
+            var mutations = MutateSrcCommand(srcCommand).ToHashSet();
+            result[srcCommand] = mutations;
+        }
+
+        return result;
+    }
+
+    private IEnumerable<string> MutateSrcCommand(string srcCommand)
+    {
+        var command = $"A{srcCommand}A";
+
+        var resultPaths = new string[][]
+        {
+            ["E"],
+            ["E"],
+            ["E"],
+            ["E"],
+            ["E"],
+        };
+
+        for (var i = 1; i < command.Length; i++)
+        {
+            var paths = GetPaths(command[i - 1], command[i]);
+            resultPaths[i - 1] = paths.ToArray();
+        }
+
+        //max 5
+        foreach (var p0 in resultPaths[0])
+        foreach (var p1 in resultPaths[1])
+        foreach (var p2 in resultPaths[2])
+        foreach (var p3 in resultPaths[3])
+        foreach (var p4 in resultPaths[4])
+        {
+            yield return new[] { p0, p1, p2, p3, p4 }.Where(x => x != "E").JoinToString("A") + "A";
+        }
+    }
+
 
     //("140A", "^<<A^A>vvA>A"),//^<<A^A>vvA>A
     // ("169A", "^<<A^>>A^AvvvA"),//^<<A^>>A^AvvvA
@@ -229,9 +285,9 @@ public class Task21_2
                     var r = keyboard.Exec(c);
                     if (r == 'E') break;
                 }
-                
+
                 if (keyboard.GetPosition() == target)
-                    mutated.Add(mutateCmd);   
+                    mutated.Add(mutateCmd);
             }
 
             newCommands.Add(mutated);
@@ -242,7 +298,7 @@ public class Task21_2
         foreach (var c2 in newCommands[2])
         foreach (var c3 in newCommands[3])
         {
-           yield return (command.Number, $"{c0}A{c1}A{c2}A{c3}A");
+            yield return (command.Number, $"{c0}A{c1}A{c2}A{c3}A");
         }
     }
 
@@ -255,19 +311,29 @@ public class Task21_2
 
         if (deepLevel == 0) return cmd.Length + 1L;
 
-        var nextCommands = Commands[cmd];
-        var result = 0L;
-        var cmds = nextCommands.Split('A');
-        for (var i = 0; i < cmds.Length - 1; i++)
+        var nextCommands = CommandsMutated[cmd];
+        
+        var result = long.MaxValue;
+
+        foreach (var nextCommand in nextCommands)
         {
-            var nextCmd = cmds[i];
-            result += GetLength(nextCmd, deepLevel - 1, cache);
+            var internalResult = 0L;
+            var cmds = nextCommand.Split('A');
+            for (var i = 0; i < cmds.Length - 1; i++)
+            {
+                var nextCmd = cmds[i];
+                internalResult += GetLength(nextCmd, deepLevel - 1, cache);
+            }
+
+            if (internalResult < result) result = internalResult;
         }
         
         cache[(cmd, deepLevel)] = result;
 
         return result;
     }
+
+    private static Dictionary<string, HashSet<string>> CommandsMutated = new();
 
     private static readonly Dictionary<string, string> Commands = new()
     {
@@ -312,7 +378,7 @@ public class Task21_2
         { "<^^<", "v<<A>^AAv<A>>^A" },
         { "<^<^", "v<<A>^Av<A>^A>A" },
     };
-    
+
     private static IEnumerable<string> GetAllCommands()
     {
         var verticals = new[] { "", "v", "^" };
@@ -336,20 +402,20 @@ public class Task21_2
         };
 
         var aS = new[] { "A", "AA", "AAA" };
-        
+
         foreach (var horizontal in commands)
         foreach (var a in aS)
         {
             yield return $"{horizontal}{a}";
         }
     }
-    
+
     //029A: <vA<AA>>^AvAA<^A>A<v<A>>^AvA^A<vA>^A<v<A>^A>AAvA^A<v<A>A>^AAAvA<^A>A
     //980A: <v<A>>^AAAvA^A<vA<AA>>^AvAA<^A>A<v<A>A>^AAAvA<^A>A<vA>^A<A>A
     //179A: <v<A>>^A<vA<A>>^AAvAA<^A>A<v<A>>^AAvA^A<vA>^AA<A>A<v<A>A>^AAAvA<^A>A
     //456A: <v<A>>^AA<vA<A>>^AAvAA<^A>A<vA>^A<A>A<vA>^A<A>A<v<A>A>^AAvA<^A>A
     //379A: <v<A>>^AvA^A<vA<AA>>^AAvA<^A>AAvA^A<vA>^AA<A>A<v<A>A>^AAAvA<^A>A
-    
+
     [Test]
     [TestCase("<vA<AA>>^AvAA<^A>A<v<A>>^AvA^A<vA>^A<v<A>^A>AAvA^A<v<A>A>^AAAvA<^A>A", "029A")]
     [TestCase("v<A<AA>>^AvAA^<A>A<v<A>>^AvA^A<v<A>>^AAvA<A>^A<A>Av<A<A>>^AAA<A>vA^A", "029A")]
@@ -383,7 +449,7 @@ public class Task21_2
         var actual = mutations.Select(x => new string(x.ToArray())).Distinct().ToArray();
         actual.OrderBy(x => x).ToArray().Should().BeEquivalentTo(expected.SplitLines().OrderBy(x => x).ToArray());
     }
-    
+
     private static string ExecCommand(string command, Keyboard keyboard)
     {
         var result = string.Empty;
@@ -392,7 +458,7 @@ public class Task21_2
             var r = keyboard.Exec(c);
 
             if (r == 'E') throw new Exception("Dsdsad");
-            
+
             if (r != default)
                 result += r;
         }
@@ -416,7 +482,7 @@ public class Task21_2
     }
 
     private static NumericKeyboard Numeric;
-    
+
     private class NumericKeyboard
     {
         private char[][] Map { get; } =
@@ -434,7 +500,7 @@ public class Task21_2
             var point = Map.Find(c);
             Position = point;
         }
-        
+
         public char GetPosition()
         {
             return Map.Get(Position);
@@ -466,6 +532,91 @@ public class Task21_2
             Position += step;
 
             return c;
+        }
+    }
+
+    public static IEnumerable<string> GetPaths(char fromChar, char toChar)
+    {
+        if (fromChar == toChar)
+        {
+            yield return "";
+            yield break;
+        }
+
+        switch (fromChar, toChar)
+        {
+            case ('A', '^'):
+                yield return "<";
+                break;
+            case ('A', '<'):
+                yield return "<v<";
+                yield return "v<<";
+                break;
+            case ('A', '>'):
+                yield return "v";
+                break;
+            case ('A', 'v'):
+                yield return "v<";
+                yield return "<v";
+                break;
+
+            case ('^', 'A'):
+                yield return ">";
+                break;
+            case ('^', '<'):
+                yield return "v<";
+                break;
+            case ('^', '>'):
+                yield return "v>";
+                yield return ">v";
+                break;
+            case ('^', 'v'):
+                yield return "v";
+                break;
+
+            case ('<', '^'):
+                yield return ">^";
+                break;
+            case ('<', 'A'):
+                yield return ">>^";
+                yield return ">^>";
+                break;
+            case ('<', '>'):
+                yield return ">>";
+                break;
+            case ('<', 'v'):
+                yield return ">";
+                break;
+
+            case ('>', '^'):
+                yield return "<^";
+                yield return "^<";
+                break;
+            case ('>', '<'):
+                yield return "<<";
+                break;
+            case ('>', 'A'):
+                yield return "^";
+                break;
+            case ('>', 'v'):
+                yield return "<";
+                break;
+
+            case ('v', '^'):
+                yield return "^";
+                break;
+            case ('v', '<'):
+                yield return "<";
+                break;
+            case ('v', '>'):
+                yield return ">";
+                break;
+            case ('v', 'A'):
+                yield return ">^";
+                yield return "^>";
+                break;
+
+            default: throw new Exception("Dsaddsa");
         }
     }
 
@@ -508,11 +659,11 @@ public class Task21_2
                     else return Numeric.Exec(Map.Get(Position));
                 default: throw new Exception("key unknown");
             }
-            
+
             if (Map.SafeGet(Position + step) == default) return 'E';
-            
+
             Position += step;
-            
+
             return default;
         }
     }
